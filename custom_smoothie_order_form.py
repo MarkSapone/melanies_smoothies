e68a2294b5e322cf15918157370ce14cf06bc7bc
MarkSapone <shopping@seife.com>
2025-04-27 20:05:15 -0400

    Update custom_smoothie_order_form.py

--- custom_smoothie_order_form.py
+++ custom_smoothie_order_form.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
+import requests
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -30,6 +30,9 @@ ingredients_string = ''
 
 for each_fruit in ingredients_list:
     ingredients_string += each_fruit + ' ' 
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
@@ -41,7 +44,5 @@ if time_to_insert:
     st.success('Your Smoothie is ordered!', icon="✅")
 
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
-sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+

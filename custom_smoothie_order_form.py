60136ca7657af52b6e8e6717e2cf858efeeb6830
MarkSapone <shopping@seife.com>
2025-04-27 20:16:55 -0400

    Update custom_smoothie_order_form.py

--- custom_smoothie_order_form.py
+++ custom_smoothie_order_form.py
@@ -17,9 +17,13 @@ name_on_order = st.text_input("Name on Smoothie")
 st.write("The name will be: ", name_on_order)
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
 
+#convert the ST dataframe to PD
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 
 ingredients_list = st.multiselect('Choose up to five ingredients:', my_dataframe, max_selections=5)
